56e4e24be676749d8394a7fef1cf75ac37ae50a3
Ricardo Vieira <ricardo.vieira@xpand-it.com>
2017-05-29 11:21:47 +0100

    1 - adding sampleFile to the project

---
+++ sample/src/main/java/com/rjsvieira/floatingmenu/sample/MainActivity.java
@@ -0,0 +1,57 @@
+package com.rjsvieira.floatingmenu.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+
+import rjsv.floatingmenu.animation.enumerators.AnimationType;
+import rjsv.floatingmenu.floatingmenubutton.FloatingMenuButton;
+
+public class MainActivity extends Activity {
+
+    FloatingMenuButton fab_1;
+    FloatingMenuButton fab_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initUi();
+    }
+
+    private void initUi(){
+        fab_1 = (FloatingMenuButton) findViewById(R.id.fab_1);
+        fab_1.setStartAngle(0)
+                .setEndAngle(360)
+                .setAnimationType(AnimationType.EXPAND)
+                .setAnchored(false);
+        fab_1.getAnimationHandler()
+                .setOpeningAnimationDuration(500)
+                .setClosingAnimationDuration(200)
+                .setLagBetweenItems(0)
+                .setOpeningInterpolator(new FastOutSlowInInterpolator())
+                .setClosingInterpolator(new FastOutLinearInInterpolator())
+                .shouldFade(true)
+                .shouldScale(true)
+                .shouldRotate(false)
+        ;
+
+        fab_2 = (FloatingMenuButton) findViewById(R.id.fab_2);
+        fab_2.setStartAngle(0)
+                .setEndAngle(360)
+                .setAnimationType(AnimationType.RADIAL)
+                .setAnchored(false);
+        fab_2.getAnimationHandler()
+                .setOpeningAnimationDuration(500)
+                .setClosingAnimationDuration(200)
+                .setLagBetweenItems(0)
+                .setOpeningInterpolator(new FastOutSlowInInterpolator())
+                .setClosingInterpolator(new FastOutLinearInInterpolator())
+                .shouldFade(true)
+                .shouldScale(true)
+                .shouldRotate(false)
+        ;
+
+    }
+}
